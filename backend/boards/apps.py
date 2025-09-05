e665f01dd39f4ce1953d9abb6666cfbb0c5af8e3
Jingrui Cheng <2994180c@student.gla.ac.uk>
2025-09-05 13:29:49 +0100

    Submission/work

--- backend/boards/apps.py
+++ backend/boards/apps.py
@@ -1,15 +1,8 @@
 from django.apps import AppConfig
 
-<<<<<<< HEAD
-
-class BoardsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boards'
-=======
 class BoardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boards'
 
     def ready(self):
         import boards.signals  # noqa: F401 - Import to connect signals
->>>>>>> gitlab/tripboard-import
--- backend/manage.py
+++ backend/manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelboard.settings')
-=======
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travelkanban.settings')
->>>>>>> gitlab/tripboard-import
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
